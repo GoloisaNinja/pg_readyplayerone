2b5ffbb235cb0d84dc681a6672e4fec6f7fe10fd
Golodhrim <holidayninjastaff@gmail.com>
2023-06-16 16:55:05 -0400

    db config update to include deployed prod database uri

--- db/config.js
+++ db/config.js
@@ -1,24 +1,30 @@
 const {Sequelize, Model, DataTypes} = require('sequelize');
 
 const connect = () => {
-    const hostName = process.env.HOST;
-    const userName = process.env.USER;
-    const password = process.env.PASSWORD;
-    const database = process.env.DB;
-    const dialect = process.env.DIALECT
 
+    // DEV MANUAL SEQUELIZE WITH PARAMS
 
-    const sequelize = new Sequelize(database, userName, password, {
-        host: hostName,
-        dialect,
-        pool: {
-            max: 10,
-            min: 0,
-            acquire: 20000,
-            idle: 5000,
-        },
-        define: { timestamps: false }
-    });
+    // const hostName = process.env.PGHOST;
+    // const userName = process.env.PGUSER;
+    // const password = process.env.PGPASSWORD;
+    // const database = process.env.PGDATABASE;
+    // const dialect = process.env.DIALECT
+
+    // const sequelize = new Sequelize(database, userName, password, {
+    //     host: hostName,
+    //     dialect,
+    //     pool: {
+    //         max: 10,
+    //         min: 0,
+    //         acquire: 20000,
+    //         idle: 5000,
+    //     },
+    //     define: { timestamps: false }
+    // });
+
+    // PROD SEQUELIZE WITH CONNECTION URI
+
+    const sequelize = new Sequelize(process.env.DATABASE_URL);
 
     const db = {};
     db.Sequelize = Sequelize;
